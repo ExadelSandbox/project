e8e680775f685563370658e8fd79d38199680655
Artem Gael <50806966+artemgael96@users.noreply.github.com>
2021-07-06 16:46:51 +0300

    change webapi
    change webapi(moved)

--- InternshipBE/WebApi/Program.cs
+++ InternshipBE/WebApi/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace InternshipBE
+namespace WebApi
 {
     public class Program
     {
--- InternshipBE/WebApi/Startup.cs
+++ InternshipBE/WebApi/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace InternshipBE
+namespace WebApi
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace InternshipBE
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "InternshipBE", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace InternshipBE
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InternshipBE v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
 
             app.UseHttpsRedirection();

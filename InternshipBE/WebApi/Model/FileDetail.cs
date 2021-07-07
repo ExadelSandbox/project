da4a46526774c08750331c556b54e42814d425ec
vlad12345123 <vzezjulin@gmail.com>
2021-07-07 18:06:10 +0300

    Implementing project architecture
    Implementing project architecture and basics things whats we need: EF core, Controllers, some patterns. Also the implementation of working with files

---
+++ InternshipBE/WebApi/Model/FileDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class FileDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public Format Formats { get; set; }
+
+        public enum Format
+        {
+            Mp3,
+            Wma
+        }
+    }
+}

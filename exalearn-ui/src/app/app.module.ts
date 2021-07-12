fbe6918b9e87b6120ed10b6b52389887e3574507
ozhurova <ozhurchik@gmail.com>
2021-07-12 21:11:25 +0300

    Create Footer Component

--- exalearn-ui/src/app/app.module.ts
+++ exalearn-ui/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, FooterComponent],
 	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent]

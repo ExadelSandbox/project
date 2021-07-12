39bb7f55ada4c8a2fbdc98d7569d2de2a45f4181
vlad12345123 <vzezjulin@gmail.com>
2021-07-12 17:37:26 +0300

    fix

---
+++ InternshipBE/ExaLearn.Shared/Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace Portal.Core.Constants
+{
+    public static class RoleNames
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Hr = "Hr";
+        public const string Coach = "Coach";
+    }
+}

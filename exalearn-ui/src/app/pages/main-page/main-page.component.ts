57ff8a4329991fed86a303c71ac160078229f33d
mityajs <dmnatest1@gmail.com>
2021-07-12 18:23:40 +0300

    [2-13] Main page redesign; refactoring

--- exalearn-ui/src/app/pages/main-page/main-page.component.ts
+++ exalearn-ui/src/app/pages/main-page/main-page.component.ts
@@ -5,14 +5,14 @@ import { RedirectBtn } from '../../interfaces/interfaces';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.less']
+  styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
   btns: readonly RedirectBtn[] = []
   constructor(private roleService: RolesService) { }
 
   ngOnInit(): void {
-    this.btns = this.roleService.getBtns("user")
+    this.btns = this.roleService.getBtns("coach")
   }
 
 }

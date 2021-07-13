62223c569c837e87fa9373549ad35d9cc09d80da
vlad12345123 <vzezjulin@gmail.com>
2021-07-13 22:15:16 +0300

    implement Users

---
+++ InternshipBE/ExaLearn.Bl/Interfaces/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using ExaLearn.Dal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaLearn.Bl.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<User> AddAsync(User user);
+
+        public Task<List<User>> GetAllAsync();
+
+        public Task<User> GetAsync(int id);
+
+        public Task<User> Remove(User user);
+
+        public Task<User> Update(User user);
+    }
+}

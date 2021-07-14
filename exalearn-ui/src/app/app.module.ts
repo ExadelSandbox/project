eba3a00e4e12240cf2fe279fd7ae5518d2ab77f6
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-07-14 12:42:49 +0500

    Added separate module for Material Components

--- exalearn-ui/src/app/app.module.ts
+++ exalearn-ui/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MainPageModule } from './pages/main-page/main-page.module';
+import { MaterialModule } from './modules/material/material.module';
 
 import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { EssayPartComponent } from './components/essay-part/essay-part.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-	declarations: [AppComponent, EssayPartComponent],
-	imports: [BrowserModule, AppRoutingModule, MainPageModule],
+	declarations: [AppComponent, MainPageComponent, HeaderComponent],
+	imports: [BrowserModule, AppRoutingModule, MatButtonModule, MaterialModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })

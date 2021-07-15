8fc914c4eb36f4942b4adf3ace26db32d37fe944
KotdenGit <kotden09@gmail.com>
2021-07-15 15:37:39 +0300

    [2-19]init: add files for essay-part component

--- exalearn-ui/src/app/app.module.ts
+++ exalearn-ui/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { MainPageModule } from './pages/main-page/main-page.module';
 
 import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { EssayPartComponent } from './components/essay-part/essay-part.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, EssayPartComponent],
 	imports: [BrowserModule, AppRoutingModule, MainPageModule],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ exalearn-ui/src/app/components/essay-part/essay-part.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-essay-part',
+	templateUrl: './essay-part.component.html',
+	styleUrls: ['./essay-part.component.scss']
+})
+export class EssayPartComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}

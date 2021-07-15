4d3fce0b9e18e55a290022a8b5e0b89f2f8b19f0
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-07-15 18:07:47 +0500

    [2-5] added routerLinks to buttons; "pass test" leads to Test page

--- exalearn-ui/src/app/app.module.ts
+++ exalearn-ui/src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainPageModule } from './pages/main-page/main-page.module';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './pages/main-page/main-page.component';
-import { MatButtonModule } from '@angular/material/button';
-import { HeaderComponent } from './components/header/header.component';
-import { TestPageComponent } from './pages/test-page/test-page.component';
-import { TimerComponent } from './components/timer/timer.component';
-import { TestPageModule } from './pages/test-page/test-page.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, MainPageModule],
-	declarations: [AppComponent, MainPageComponent, HeaderComponent, TestPageComponent, TimerComponent],
-	imports: [BrowserModule, AppRoutingModule, MatButtonModule, BrowserAnimationsModule, TestPageModule],
+	imports: [BrowserModule, AppRoutingModule, MainPageModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })

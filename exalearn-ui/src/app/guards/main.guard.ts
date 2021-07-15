50005f053cfa21df91526a502a288ab8730418dc
HenadzV <henadzv@yandex.by>
2021-07-15 23:05:43 +0300

    [2-4] Add guard for main-page

---
+++ exalearn-ui/src/app/guards/main.guard.ts
@@ -0,0 +1,20 @@
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { from, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class MainGuard implements CanActivate {
+	constructor(private authService: AuthService, private router: Router) {}
+
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+		return from(this.authService.isAuthenticated()).pipe(
+			map((isAuth) => {
+				if (isAuth) return true;
+				this.router.navigateByUrl('/login');
+				return false;
+			})
+		);
+	}
+}
--- exalearn-ui/src/app/modules/app-routing/app-routing.module.ts
+++ exalearn-ui/src/app/modules/app-routing/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from '../../pages/main-page/main-page.component';
 import { LoginPageComponent } from '../../pages/login-page/login-page.component';
 import { AuthGuard } from '../../guards/auth.guard';
+import { MainGuard } from '../../guards/main.guard';
 
 const routes: Routes = [
-	{ path: 'main', component: MainPageComponent },
+	{ path: 'main', component: MainPageComponent, canActivate: [MainGuard] },
 	{ path: '', redirectTo: '/login', pathMatch: 'full' },
 	{ path: 'login', component: LoginPageComponent, canActivate: [AuthGuard] }
 ];

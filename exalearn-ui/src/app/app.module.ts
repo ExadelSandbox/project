7b8d3a79bb8cc82e20c068636340caf59ba550d4
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-07-15 20:28:31 +0500

    [2-5] modified based on feedback

--- exalearn-ui/src/app/app.module.ts
+++ exalearn-ui/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainPageModule } from './pages/main-page/main-page.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, MainPageModule, BrowserAnimationsModule],
+	imports: [BrowserModule, AppRoutingModule, MainPageModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })

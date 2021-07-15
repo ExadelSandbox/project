ecc9c30e155bed6a50b372e316407ef12364d39d
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-07-15 14:39:20 +0500

    [2-5] renamed classes and fixed selector of test-page

--- exalearn-ui/src/app/pages/test-page/test-page.component.ts
+++ exalearn-ui/src/app/pages/test-page/test-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-test-main',
+	selector: 'app-test-page',
 	templateUrl: './test-page.component.html',
 	styleUrls: ['./test-page.component.scss']
 })

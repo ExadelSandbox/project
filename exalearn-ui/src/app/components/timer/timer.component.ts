bd04a666a9aad68aab41e5ffa25ea03bdd73d4b4
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-07-15 20:28:31 +0500

    [2-5] modified based on feedback

--- exalearn-ui/src/app/components/timer/timer.component.ts
+++ exalearn-ui/src/app/components/timer/timer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 // Test time is set to 1 hour (in seconds)
+// TODO: give the ability to set the time duration through a custom attribute
 const TEST_DURATION = 3600;
 
 @Component({
@@ -11,6 +12,7 @@ const TEST_DURATION = 3600;
 export class TimerComponent implements OnInit {
 	mins: string = '60';
 	secs: string = '00';
+	timer: number = TEST_DURATION;
 
 	constructor() {}
 
@@ -19,28 +21,28 @@ export class TimerComponent implements OnInit {
 	}
 
 	startTimer() {
-		let timer = TEST_DURATION;
+		const timerInterval = setInterval(() => {
+			this.displayTimeLeft(timerInterval);
+		}, 1000);
+	}
 
-		const displayTimeLeft = () => {
-			let minutes = Math.floor(timer / 60),
-				seconds = timer % 60;
+	displayTimeLeft(interval: any) {
+		let minutes = Math.floor(this.timer / 60),
+			seconds = this.timer % 60;
 
-			const objTime = this.formatTime(minutes, seconds);
+		const objTime = this.formatTime(minutes, seconds);
 
-			timer--;
+		this.timer--;
 
-			// if timer is below 0, set min/sec to '00' and clear interval
-			if (timer < 0) {
-				this.mins = '00';
-				this.secs = '00';
-				clearInterval(timerInterval);
-			} else {
-				this.mins = objTime.mins;
-				this.secs = objTime.secs;
-			}
-		};
-
-		const timerInterval = setInterval(displayTimeLeft, 1000);
+		// if timer is below 0, set min/sec to '00' and clear interval
+		if (this.timer < 0) {
+			this.mins = '00';
+			this.secs = '00';
+			clearInterval(interval);
+		} else {
+			this.mins = objTime.mins;
+			this.secs = objTime.secs;
+		}
 	}
 
 	// Add zero to mins/secs if they are below 10;
--- exalearn-ui/src/app/pages/test-page/test-page.module.ts
+++ exalearn-ui/src/app/pages/test-page/test-page.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TestPageComponent } from './test-page.component';
 import { TimerComponent } from '../../components/timer/timer.component';
 
 @NgModule({
 	declarations: [TestPageComponent, TimerComponent],
-	imports: [MatTabsModule]
+	imports: [MatTabsModule, BrowserAnimationsModule],
+	exports: [TestPageComponent]
 })
 export class TestPageModule {}

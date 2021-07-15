850001ced195d9d0add78da039a0d56633781f1f
KotdenGit <kotden09@gmail.com>
2021-07-15 23:23:21 +0300

    [2-19]feat: connecting the essay module to the test page

--- exalearn-ui/src/app/pages/test-page/test-page.module.ts
+++ exalearn-ui/src/app/pages/test-page/test-page.module.ts
@@ -5,9 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestPageComponent } from './test-page.component';
 import { TimerComponent } from '../../components/timer/timer.component';
 import { TestGrammarModule } from '../../components/test-grammar/test-grammar.module';
+import { EssayPartComponent } from '../../components/essay-part/essay-part.component';
 
 @NgModule({
-	declarations: [TestPageComponent, TimerComponent],
+	declarations: [TestPageComponent, EssayPartComponent, TimerComponent],
 	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule],
 	exports: [TestPageComponent]
 })

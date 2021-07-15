4de7df8d6b91556469915570b57e9f2906c2bc7a
KotdenGit <kotden09@gmail.com>
2021-07-15 15:37:39 +0300

    [2-19]init: add files for essay-part component

--- exalearn-ui/src/app/app.module.ts
+++ exalearn-ui/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { MainPageModule } from './pages/main-page/main-page.module';
 
 import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { EssayPartComponent } from './components/essay-part/essay-part.component';
 import { LoginPageModule } from './pages/login-page/login-page.module';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, EssayPartComponent],
 	imports: [BrowserModule, AppRoutingModule, MainPageModule, LoginPageModule],
 	providers: [],
 	bootstrap: [AppComponent]

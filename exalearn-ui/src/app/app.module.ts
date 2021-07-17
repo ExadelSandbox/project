0c1175514d1ea91c55e2e5b927a9a7b9898691e0
Yury Hreben <urikgreben@gmail.com>
2021-07-17 18:50:12 +0300

    [2-37] UI: Rebase header and footer

--- exalearn-ui/src/app/app.module.ts
+++ exalearn-ui/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { MainPageModule } from './pages/main-page/main-page.module';
 import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageModule } from './pages/login-page/login-page.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, MainPageModule, LoginPageModule],
+	declarations: [AppComponent, HeaderComponent, FooterComponent],
+	imports: [BrowserModule, AppRoutingModule, MainPageModule, LoginPageModule, MatButtonModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
--- exalearn-ui/src/app/pages/main-page/main-page.module.ts
+++ exalearn-ui/src/app/pages/main-page/main-page.module.ts
@@ -5,11 +5,9 @@ import { AppRoutingModule } from '../../modules/app-routing/app-routing.module';
 import { TestPageModule } from '../test-page/test-page.module';
 
 import { MainPageComponent } from './main-page.component';
-import { HeaderComponent } from '../../components/header/header.component';
-import { FooterComponent } from '../../components/footer/footer.component';
 
 @NgModule({
-	declarations: [MainPageComponent, HeaderComponent, FooterComponent],
+	declarations: [MainPageComponent],
 	imports: [BrowserModule, MatButtonModule, TestPageModule, AppRoutingModule],
 	providers: [],
 	bootstrap: []

5bc755a480c1d5d8b89ec741975e71f00faeb4ec
Artem Gael <50806966+artemgael96@users.noreply.github.com>
2021-07-19 19:42:25 +0300

    changed middleware UseSwagger, UseSwaggerUI
    changed middleware UseSwagger, UseSwaggerUI

--- exalearn-api/ExaLearn.WebApi/Startup.cs
+++ exalearn-api/ExaLearn.WebApi/Startup.cs
@@ -107,9 +107,10 @@ namespace ExaLearn.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
 
             app.UseGlobalExceptionMiddleware();
 

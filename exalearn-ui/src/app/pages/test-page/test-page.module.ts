fc9d2b714723810b093831ab08d66cca72f724c4
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-07-19 13:27:15 +0300

    Testing

--- exalearn-ui/src/app/pages/test-page/test-page.module.ts
+++ exalearn-ui/src/app/pages/test-page/test-page.module.ts
@@ -6,11 +6,12 @@ import { MatInputModule } from '@angular/material/input';
 import { TestPageComponent } from './test-page.component';
 import { TimerComponent } from '../../components/timer/timer.component';
 import { TestGrammarModule } from '../../components/test-grammar/test-grammar.module';
+import { AuditionModule } from '../../components/audition/audition.module';
 import { EssayPartComponent } from '../../components/essay-part/essay-part.component';
 
 @NgModule({
 	declarations: [TestPageComponent, EssayPartComponent, TimerComponent],
-	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule, MatInputModule],
+	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule, MatInputModule, AuditionModule],
 	exports: [TestPageComponent]
 })
 export class TestPageModule {}

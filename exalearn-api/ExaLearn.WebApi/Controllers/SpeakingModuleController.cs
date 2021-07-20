5d3ccdcaf579e1ced69d785819c90d5f46969f8c
Alpaslan <alpaslanpolat2503@gmail.com>
2021-07-20 21:41:05 +0500

    Created basic Speaking module controller

---
+++ exalearn-api/ExaLearn.WebApi/Controllers/SpeakingModuleController.cs
@@ -0,0 +1,31 @@
+﻿using ExaLearn.Bl.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ExaLearn.WebApi.Controllers
+{
+    [Route("api/speaking")]
+    [ApiController]
+    [Authorize]
+    public class SpeakingModuleController : ControllerBase
+    {
+        private readonly IAudioFileService _audioFileService;
+
+        public SpeakingModuleController(IAudioFileService audioFileService)
+        {
+            _audioFileService = audioFileService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitSpeaking(IFormFile file) //speaker's audio file
+        {
+            var audioAnswer = await _audioFileService.CreateAsync(file);
+            //send file url to the coach controller
+
+            return Ok();
+        }
+        
+    }
+}

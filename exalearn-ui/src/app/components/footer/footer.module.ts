6162ac36c3c4a9b10708e61f71b7a8c77c802b10
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-07-21 16:03:59 +0300

    [3-6] Adding translation to project. Refactored code.

--- exalearn-ui/src/app/components/footer/footer.module.ts
+++ exalearn-ui/src/app/components/footer/footer.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FooterComponent } from './footer.component';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
 	declarations: [FooterComponent],
-	imports: [CommonModule, TranslateModule],
+	imports: [CommonModule, TranslateModule, MatSelectModule],
 	providers: [],
 	exports: [FooterComponent]
 })

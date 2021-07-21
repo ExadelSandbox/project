68519c76ad9c0cb2cd0363d827655c8afc4b16d8
KotdenGit <kotden09@gmail.com>
2021-07-21 16:23:42 +0300

    [3-30]init: start options tuning plus essay-part.module

--- exalearn-ui/src/app/pages/test-page/test-page.module.ts
+++ exalearn-ui/src/app/pages/test-page/test-page.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
 
 import { TestPageComponent } from './test-page.component';
 import { TimerComponent } from '../../components/timer/timer.component';
 import { TestGrammarModule } from '../../components/test-grammar/test-grammar.module';
 import { AuditionModule } from '../../components/audition/audition.module';
-import { EssayPartComponent } from '../../components/essay-part/essay-part.component';
+import { EssayPartModule } from '../../components/essay-part/essay-part.module';
 
 @NgModule({
-	declarations: [TestPageComponent, EssayPartComponent, TimerComponent],
-	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule, MatInputModule, AuditionModule],
+	declarations: [TestPageComponent, TimerComponent],
+	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule, EssayPartModule, AuditionModule],
 	exports: [TestPageComponent]
 })
 export class TestPageModule {}

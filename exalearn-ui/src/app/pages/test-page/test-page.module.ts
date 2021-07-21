5157b22e71dcbe338865d3f85c8367d1f9dad312
ozhurova <ozhurchik@gmail.com>
2021-07-21 18:43:54 +0300

    [2-18] update test-page module

--- exalearn-ui/src/app/pages/test-page/test-page.module.ts
+++ exalearn-ui/src/app/pages/test-page/test-page.module.ts
@@ -7,10 +7,11 @@ import { TestPageComponent } from './test-page.component';
 import { TimerComponent } from '../../components/timer/timer.component';
 import { TestGrammarModule } from '../../components/test-grammar/test-grammar.module';
 import { EssayPartComponent } from '../../components/essay-part/essay-part.component';
+import { SpeakingModule } from '../../components/speaking/speaking.module';
 
 @NgModule({
 	declarations: [TestPageComponent, EssayPartComponent, TimerComponent],
-	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule, MatInputModule],
+	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule, MatInputModule, SpeakingModule],
 	exports: [TestPageComponent]
 })
 export class TestPageModule {}

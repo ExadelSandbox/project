cfd9acdca506e29a2c6a0d3e6824d7e4c31d9437
KotdenGit <kotden09@gmail.com>
2021-07-22 00:19:09 +0300

    [3-30]refactor: change structure test-page.module.ts

--- exalearn-ui/src/app/pages/test-page/test-page.module.ts
+++ exalearn-ui/src/app/pages/test-page/test-page.module.ts
@@ -6,20 +6,17 @@ import { TestPageComponent } from './test-page.component';
 import { TimerComponent } from '../../components/timer/timer.component';
 import { TestGrammarModule } from '../../components/test-grammar/test-grammar.module';
 import { AuditionModule } from '../../components/audition/audition.module';
-import { EssayPartComponent } from '../../components/essay-part/essay-part.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { EssayPartModule } from '../../components/essay-part/essay-part.module';
 
 @NgModule({
 	declarations: [TestPageComponent, TimerComponent],
-	imports: [MatTabsModule, BrowserAnimationsModule, TestGrammarModule, EssayPartModule, AuditionModule],
-	declarations: [TestPageComponent, EssayPartComponent, TimerComponent],
 	imports: [
 		MatTabsModule,
 		BrowserAnimationsModule,
 		TestGrammarModule,
-		MatInputModule,
 		AuditionModule,
+		EssayPartModule,
 		TranslateModule
 	],
 	exports: [TestPageComponent]

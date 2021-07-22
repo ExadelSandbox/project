01f0dca786aab32e548b023b5e49bc1a27d2b729
KuznetsovR <abcruslan2004@mail.ru>
2021-07-22 18:00:30 +0500

    [2-36] all history module added to main page

--- exalearn-ui/src/app/pages/main-page/main-page.module.ts
+++ exalearn-ui/src/app/pages/main-page/main-page.module.ts
@@ -9,6 +9,7 @@ import { FooterModule } from '../../components/footer/footer.module';
 import { HeaderModule } from '../../components/header/header.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { MenuModule } from '../../components/menu/menu.module';
+import { AllHistoryPageModule } from '../all-history-page/all-history-page.module';
 
 @NgModule({
 	declarations: [MainPageComponent],

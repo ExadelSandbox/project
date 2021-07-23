a5ac9e9c4f3a2fbb204aeab8f108f3f9f4705aec
ozhurova <ozhurchik@gmail.com>
2021-07-23 18:19:30 +0300

    [3-8] add Media Recorder

--- exalearn-ui/src/app/components/speaking/speaking.component.ts
+++ exalearn-ui/src/app/components/speaking/speaking.component.ts
@@ -1,32 +1,52 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AudioFile } from '../../interfaces/interfaces';
+import { Component, OnInit } from '@angular/core';
+
+declare const MediaRecorder: any;
 
 @Component({
 	selector: 'app-speaking',
 	templateUrl: './speaking.component.html',
 	styleUrls: ['./speaking.component.scss']
 })
-export class SpeakingComponent {
-	@Input() audio: AudioFile;
+export class SpeakingComponent implements OnInit {
+	public topic = 'What is happiness?';
+	private mediaRecorder: any;
+	private chunks: Blob[] = [];
 
-	declare MediaRecorder: any;
-	topic: string = 'What is happiness?';
+	readonly recordingDuration: number = 300000;
+	recording: boolean;
+	recorder: Promise<MediaStream>;
 
-	getConstraint(): void {
-		const constraints: any = { audio: true };
-		navigator.mediaDevices
-			.getUserMedia(constraints)
-			.then(function (stream) {
-				const mediaRecorder = new MediaRecorder(stream);
-				let audio = document.querySelector('audio');
-				audio.srcObject = stream;
-				audio.onloadedmetadata = function () {
-					audio.play();
-					audio.muted = true;
-				};
-			})
-			.catch(function (err) {
-				console.log(false);
-			});
+	ngOnInit(): void {
+		this.recorder = navigator.mediaDevices.getUserMedia({ audio: true });
+		this.recording = false;
+	}
+
+	startRecording(): void {
+		void this.recorder.then((stream) => {
+			this.mediaRecorder = new MediaRecorder(stream);
+			this.recording = true;
+			this.mediaRecorder.start(this.recordingDuration);
+			console.log(this.mediaRecorder.mimeType);
+
+			this.chunks = [];
+			this.mediaRecorder.ondataavailable = (event: any) => {
+				console.log(this.chunks.length);
+				this.chunks.push(event.data);
+			};
+
+			this.mediaRecorder.onstop = () => {
+				const audioBlob = new Blob(this.chunks);
+				const audioUrl = URL.createObjectURL(audioBlob);
+				const audio = new Audio(audioUrl);
+				void audio.play();
+			};
+		});
+	}
+
+	stopRecording(): void {
+		if (this.recording) {
+			this.recording = false;
+			this.mediaRecorder.stop();
+		}
 	}
 }

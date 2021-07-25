a2b042246a5c614201a9fa1766bba6b061072194
Alena Burak <elenaburak@outlook.com>
2021-07-25 14:16:33 +0300

    refactor

--- exalearn-api/ExaLearn.Bl/Mapping/QuestionProfile.cs
+++ exalearn-api/ExaLearn.Bl/Mapping/QuestionProfile.cs
@@ -14,20 +14,22 @@ namespace ExaLearn.Bl.Mapping
                 .ForMember(x => x.Level, map => map.MapFrom(source => source.Type))
                 .ForMember(x => x.Question, map => map.MapFrom(source => source.Text))
                 .ForMember(x => x.Answers, map => map.MapFrom(source => source.Answers))
-                .ReverseMap();
+                .ReverseMap()
+                .ForAllOtherMembers(x => x.Ignore());
 
             CreateMap<Question, AudioQuestionDTO>()
                 .ForMember(x => x.Level, map => map.MapFrom(source => source.LevelType))
                 .ForMember(x => x.FileUrl, map => map.MapFrom(source => source.AudioFile.Url))
                 .ForMember(x => x.Question, map => map.MapFrom(source => source.Text))
                 .ForMember(x => x.Answers, map => map.MapFrom(source => source.Answers))
-                .ReverseMap();
+                .ReverseMap()
+                .ForAllOtherMembers(x => x.Ignore());
 
             CreateMap<Question, TopicQuestionDTO>()
                 .ForMember(x => x.Level, map => map.MapFrom(source => source.Type))
                 .ForMember(x => x.Topic, map => map.MapFrom(source => source.Text))
                 .ReverseMap()
-                .ForAllOtherMembers(x => x.Ignore());               
+                .ForAllOtherMembers(x => x.Ignore());
         }
     }
 }

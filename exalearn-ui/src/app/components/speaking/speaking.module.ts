44e946b01737b9cc1d4f100783841d481f726583
ozhurova <ozhurchik@gmail.com>
2021-07-25 11:52:43 +0300

    [3-8] redesign buttons, add info recording

--- exalearn-ui/src/app/components/speaking/speaking.module.ts
+++ exalearn-ui/src/app/components/speaking/speaking.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { SpeakingComponent } from './speaking.component';
-import { TimerModule } from '../timer/timer.module';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-	imports: [CommonModule, MatButtonModule, TimerModule, TranslateModule],
+	imports: [CommonModule, MatButtonModule, TranslateModule],
 	declarations: [SpeakingComponent],
 	exports: [SpeakingComponent]
 })

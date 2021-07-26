06b01aac1880e46336c0a60479e4f89a4104631b
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-07-26 15:08:50 +0300

    [3-47] Added navigate to main on submit button, and translation.

--- exalearn-ui/src/app/components/finish-test-button/finish-test-button.component.ts
+++ exalearn-ui/src/app/components/finish-test-button/finish-test-button.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-finish-test-button',
 	templateUrl: './finish-test-button.component.html',
 	styleUrls: ['./finish-test-button.component.scss']
 })
-export class FinishTestButtonComponent {}
+export class FinishTestButtonComponent {
+	constructor(private router: Router) {}
+
+	public submitTest() {
+		void this.router.navigate(['/main']);
+	}
+}
--- exalearn-ui/src/app/components/finish-test-button/finish-test-button.module.ts
+++ exalearn-ui/src/app/components/finish-test-button/finish-test-button.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FinishTestButtonComponent } from './finish-test-button.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
 	declarations: [FinishTestButtonComponent],
-	imports: [CommonModule, MatIconModule, MatButtonModule],
+	imports: [CommonModule, MatIconModule, MatButtonModule, TranslateModule],
 	exports: [FinishTestButtonComponent]
 })
 export class FinishTestButtonModule {}

a7351405f8b43ec37b300518e579ab86a31e1077
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-07-26 14:36:41 +0300

    [3-47] Deleted lyfecycle method

--- exalearn-ui/src/app/components/finish-test-button/finish-test-button.component.ts
+++ exalearn-ui/src/app/components/finish-test-button/finish-test-button.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-finish-test-button',
 	templateUrl: './finish-test-button.component.html',
 	styleUrls: ['./finish-test-button.component.scss']
 })
-export class FinishTestButtonComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit(): void {}
-}
+export class FinishTestButtonComponent {}

b0501222b78789d0fe5096ad0d924857c66b7930
KotdenGit <kotden09@gmail.com>
2021-07-28 17:12:11 +0300

    [4-14]feat: add menu and table elements

--- exalearn-ui/src/app/pages/check-test-page/check-test-page.component.ts
+++ exalearn-ui/src/app/pages/check-test-page/check-test-page.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-check-test-page',
 	templateUrl: './check-test-page.component.html',
 	styleUrls: ['./check-test-page.component.scss']
 })
-export class CheckTestPageComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit(): void {}
+export class CheckTestPageComponent {
+	tableColumns = ['level', 'date', 'view test'];
+	dataType = 'my-tests';
 }

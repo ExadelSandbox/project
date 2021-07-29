d08a61ab7670be56e9c5a5b40b44dfe152718eeb
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-07-29 20:49:25 +0300

    [4-19] Refactored finish-test-button.component.ts

--- exalearn-ui/src/app/components/finish-test-button/finish-test-button.component.ts
+++ exalearn-ui/src/app/components/finish-test-button/finish-test-button.component.ts
@@ -9,43 +9,6 @@ import { Router } from '@angular/router';
 export class FinishTestButtonComponent {
 	constructor(private router: Router) {}
 
-	// async postTest(url = '', data = {}) {
-	// 	const response = await fetch(url, {
-	// 		method: 'POST',
-	// 		mode: 'cors',
-	// 		cache: 'no-cache',
-	// 		credentials: 'same-origin',
-	// 		headers: {
-	// 			'Content-Type': 'application/json'
-	// 		},
-	// 		redirect: 'follow',
-	// 		referrerPolicy: 'no-referrer',
-	// 		body: JSON.stringify(data)
-	// 	});
-	// 	return await response.json();
-	// }
-
-	// void this.postTest('/api/testAnswer', { answer: 42 }).then((data) => {
-	// 	console.log(data);
-	// });
-
-	// 	url = '/api/testAnswer';
-	// 	data = { test: 'test' };
-	//
-	//   try {
-	//   const response = await fetch(this.url, {
-	//     method: 'POST',
-	//     body: JSON.stringify(this.data),
-	//     headers: {
-	//       'Content-Type': 'application/json'
-	//     }
-	//   });
-	//   const json = await response.json();
-	//   console.log('Успех:', JSON.stringify(json));
-	// } catch (error) {
-	//   console.error('Ошибка:', error);
-	// }
-
 	public async submitTest() {
 		void (await this.router.navigate(['/main']));
 	}

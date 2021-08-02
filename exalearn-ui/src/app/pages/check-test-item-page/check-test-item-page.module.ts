6fe145542c614fdf7ca39a49e0f0bc87dc311c75
KotdenGit <kotden09@gmail.com>
2021-08-02 23:49:38 +0300

    [4-14]feat: release switch to item check-test page

---
+++ exalearn-ui/src/app/pages/check-test-item-page/check-test-item-page.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckTestItemPageComponent } from './check-test-item-page.component';
+
+@NgModule({
+	declarations: [CheckTestItemPageComponent],
+	imports: [CommonModule],
+	exports: [CheckTestItemPageComponent]
+})
+export class CheckTestItemPageModule {}

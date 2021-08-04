851a3ca6461b51d08849ad28fc238acc589b401e
Artem Gael <50806966+artemgael96@users.noreply.github.com>
2021-08-04 21:47:23 +0300

    add Fluent Validation, add valildation for answer model

---
+++ exalearn-api/ExaLearn.Dal/Validation/AnswerValidation.cs
@@ -0,0 +1,17 @@
+﻿using ExaLearn.Dal.Entities;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ExaLearn.Dal.Validation
+{
+    public class AnswerValidator : AbstractValidator<Answer>
+    {
+        public AnswerValidator()
+        {
+            RuleFor(answer => answer.QuestionId).NotNull();
+            RuleFor(answer => answer.Question).NotNull();
+            RuleFor(answer => answer.Text).NotNull().MaximumLength(255);
+            RuleFor(answer => answer.IsCorrect).NotNull();
+        }
+    }
+}
--- exalearn-api/ExaLearn.WebApi/Startup.cs
+++ exalearn-api/ExaLearn.WebApi/Startup.cs
@@ -19,6 +19,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json;
+using FluentValidation.AspNetCore;
 
 namespace ExaLearn.WebApi
 {
@@ -34,7 +35,11 @@ namespace ExaLearn.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers()
-                .AddJsonOptions(j => j.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
+                    .AddFluentValidation( fv =>
+                    {
+                        fv.RegisterValidatorsFromAssemblyContaining<Startup>();
+                    })
+                    .AddJsonOptions(j => j.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
 
             services.AddCors();
 

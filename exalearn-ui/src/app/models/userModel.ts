81eb682b095e69ec66c3e111700dc83e983e94cb
HenadzV <henadzv@yandex.by>
2021-08-04 11:49:32 +0300

    [4-30] Added user.model

---
+++ exalearn-ui/src/app/models/userModel.ts
@@ -0,0 +1,10 @@
+export class User {
+	constructor(
+		private id: number,
+		private roleName: string,
+		private email: string,
+		private lastName: string,
+		private firstName: string,
+		private isActive: boolean
+	) {}
+}

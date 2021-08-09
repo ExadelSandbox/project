af505c7b71645d07ffe706b9ddb17e2185084a9e
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-08-09 12:44:30 +0300

    Refactored start component for user

--- exalearn-ui/src/app/components/start-content/start-content.component.ts
+++ exalearn-ui/src/app/components/start-content/start-content.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
 	selector: 'app-start-content',
 	templateUrl: './start-content.component.html',
 	styleUrls: ['./start-content.component.scss']
 })
-export class StartContentComponent {}
+export class StartContentComponent implements OnInit {
+	currentUser: any;
+	fullName: string;
+
+	constructor(public userService: UserService) {}
+
+	ngOnInit() {
+		this.currentUser = this.userService.currentUser;
+		this.fullName = `${this.currentUser.firstName} ${this.currentUser.lastName}`;
+	}
+}

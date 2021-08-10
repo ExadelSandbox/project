b6ea35a264a30b024255b1f86d3a8a98b4173789
KuznetsovR <abcruslan2004@mail.ru>
2021-08-10 15:24:13 +0500

    [5-3] My tests and all tests integration added

--- exalearn-ui/src/app/pages/all-history-page/all-history-page.component.ts
+++ exalearn-ui/src/app/pages/all-history-page/all-history-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ALL_TESTS_DATA } from '../../test-data/tables-mock-data';
+import { API_PATH } from '../../constants/api.constants';
+import { ApiService } from '../../services/api.service';
+import { PassedTest } from '../../interfaces/interfaces';
 @Component({
 	selector: 'app-all-history-page',
 	templateUrl: './all-history-page.component.html',
 	styleUrls: ['./all-history-page.component.scss']
 })
-export class AllHistoryPageComponent {
-	data = ALL_TESTS_DATA;
-	isDataAvailable = true;
+export class AllHistoryPageComponent implements OnInit {
+	// data = ALL_TESTS_DATA;
+	data: PassedTest[] = [];
+	isDataAvailable = false;
 	tableColumns = ['username', 'level', 'date', 'view test'];
+	constructor(public apiService: ApiService) {}
+	ngOnInit() {
+		this.apiService.getRequest(API_PATH.HISTORY).then((data) => {
+			console.log(data);
+			this.data = data;
+			this.isDataAvailable = true;
+		});
+	}
 }
--- exalearn-ui/src/app/pages/my-history-page/my-history-page.component.ts
+++ exalearn-ui/src/app/pages/my-history-page/my-history-page.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MY_TESTS_DATA } from '../../test-data/tables-mock-data';
+import { API_PATH } from '../../constants/api.constants';
+import { ApiService } from '../../services/api.service';
+import { UserService } from '../../services/user.service';
+import { PassedTest } from '../../interfaces/interfaces';
 
 @Component({
 	selector: 'app-my-history-page',
 	templateUrl: './my-history-page.component.html',
 	styleUrls: ['./my-history-page.component.scss']
 })
-export class MyHistoryPageComponent {
-	data = MY_TESTS_DATA;
-	isDataAvailable = true;
+export class MyHistoryPageComponent implements OnInit {
+	// data = MY_TESTS_DATA;
+	data: PassedTest[] = [];
+	isDataAvailable = false;
 	tableColumns = ['level', 'date', 'totalScore'];
+	constructor(public apiService: ApiService, public userService: UserService) {}
+	ngOnInit() {
+		this.apiService
+			.getRequest(API_PATH.USER_HISTORY, { id: this.userService.currentUser?.currentUserId })
+			.then((data) => {
+				console.log(data);
+				// const tests: PassedTest[] = [];
+				// for (const test of data) {
+				//   tests.push({
+				//
+				//   })
+				// }
+				this.data = data;
+				this.isDataAvailable = true;
+			});
+	}
 }

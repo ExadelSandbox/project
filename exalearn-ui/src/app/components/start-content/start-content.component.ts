5fb19e922e23a431cea8c26e325cd25b4c9af1b5
KuznetsovR <abcruslan2004@mail.ru>
2021-08-10 19:54:25 +0500

    [6-54] Name and english level are now shown at the main page, added english level and getters to user model, added english level to user service

--- exalearn-ui/src/app/components/start-content/start-content.component.ts
+++ exalearn-ui/src/app/components/start-content/start-content.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { EnglishLevels } from '../../enums/enums';
 
 @Component({
 	selector: 'app-start-content',
 	templateUrl: './start-content.component.html',
 	styleUrls: ['./start-content.component.scss']
 })
-export class StartContentComponent {}
+export class StartContentComponent {
+	userName: string;
+	userLevel: EnglishLevels | null;
+	constructor(public userService: UserService) {}
+	ngOnInit() {
+		if (this.userService.currentUser) {
+			this.userName = this.userService.currentUser.fullName;
+			this.userLevel = this.userService.currentUser.level;
+		}
+	}
+}

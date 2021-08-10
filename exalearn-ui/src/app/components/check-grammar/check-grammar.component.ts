dc77b9a411f28deb3fedb8ab850722fb128e364b
KotdenGit <kotden09@gmail.com>
2021-08-10 19:42:12 +0300

    [6-3]feature: add component for check-grammar form

---
+++ exalearn-ui/src/app/components/check-grammar/check-grammar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-check-grammar',
+	templateUrl: './check-grammar.component.html',
+	styleUrls: ['./check-grammar.component.scss']
+})
+export class CheckGrammarComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}

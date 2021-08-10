2cf8cf7474ee95b53ecd121fc8bc456457a5e3bc
mityajs <dmnatest1@gmail.com>
2021-08-10 13:39:01 +0300

    [6-44] Remove profile button

--- exalearn-ui/src/app/components/header/header.component.ts
+++ exalearn-ui/src/app/components/header/header.component.ts
@@ -7,9 +7,6 @@ import { AuthService } from '../../services/auth.service';
 	styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-	isAppInitialized = true;
-	isAuthorized = true;
-
 	constructor(private authService: AuthService) {}
 
 	logout(): void {

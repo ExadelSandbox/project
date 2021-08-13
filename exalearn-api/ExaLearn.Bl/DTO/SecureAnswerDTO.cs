a352c103e595aa5d330eb6a5a495415245a5e2e1
vlad12345123 <vzezjulin@gmail.com>
2021-08-13 08:25:39 +0300

    Update SecureAnswerDTO.cs

--- exalearn-api/ExaLearn.Bl/DTO/SecureAnswerDTO.cs
+++ exalearn-api/ExaLearn.Bl/DTO/SecureAnswerDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace ExaLearn.Bl.DTO
 {
-    public class SecureAnswerDTO
+    public class SecureAnswerDTO //this model is designed for safe data delivery (the user must receive a test with no answers)
     {
         public int Id { get; set; }
 

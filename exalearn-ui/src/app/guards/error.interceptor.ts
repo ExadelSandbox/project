b605460d57734b953a640238714fbd86f4905e84
HenadzV <henadzv@yandex.by>
2021-08-16 12:30:43 +0300

    [6-68] Added error handling to api service, clean up code

--- exalearn-ui/src/app/guards/error.interceptor.ts
+++ exalearn-ui/src/app/guards/error.interceptor.ts
@@ -2,23 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Router } from '@angular/router';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 	constructor(private router: Router) {}
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		console.log('request', req);
 		return next.handle(req).pipe(
-			tap(() => {
-				console.log('intercept');
-			}),
 			catchError((error: HttpErrorResponse) => {
-				console.log('Interceptor', error);
-				if (error.status === 401) {
-					console.log('Interceptor', error);
-					this.router.navigate(['/error']);
+				if (error.status === 403) {
+					void this.router.navigate(['/error']);
 				}
 				return throwError(error);
 			})

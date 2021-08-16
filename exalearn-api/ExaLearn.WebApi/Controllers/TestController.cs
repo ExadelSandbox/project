f749ac5b47d5551c91c749ef2790b7ac7f70aab3
vlad12345123 <vzezjulin@gmail.com>
2021-08-16 14:09:21 +0300

    Update TestController.cs

--- exalearn-api/ExaLearn.WebApi/Controllers/TestController.cs
+++ exalearn-api/ExaLearn.WebApi/Controllers/TestController.cs
@@ -27,7 +27,7 @@ namespace ExaLearn.WebApi.Controllers
         }
 
         [HttpGet("testAssessment")]
-        public async Task<IActionResult> TestAssessment(AssessmentDTO assessmentDTO ,int passedTestId)
+        public async Task<IActionResult> TestAssessment(AssessmentDTO assessmentDTO, int passedTestId)
         {
             return Ok(await _testService.CreateAssesmentAsync(assessmentDTO, passedTestId));
         }

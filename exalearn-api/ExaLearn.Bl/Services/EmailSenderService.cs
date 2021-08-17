a16fefe8a13ca604c7de832f3c11bddf8c974e1e
Artem Gael <50806966+artemgael96@users.noreply.github.com>
2021-08-17 22:31:06 +0300

    updated SendEmailAsync

--- exalearn-api/ExaLearn.Bl/Services/EmailSenderService.cs
+++ exalearn-api/ExaLearn.Bl/Services/EmailSenderService.cs
@@ -1,5 +1,4 @@
-﻿using ExaLearn.Bl.Interfaces;
-using System;
+﻿using System;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -10,22 +9,21 @@ namespace ExaLearn.Bl.Services
     {
         public static async Task SendEmailAsync()
         {
-            var from = new MailAddress("artem@gmail.com", "Tom");
+            var from = new MailAddress("userexa@mailnesia.com","supportExaLearn-api");
             var to = new MailAddress("userexa@mailnesia.com");
             var m = new MailMessage(from, to)
             {
-                Subject = "ТестПисьмо",
-                Body = "<h2>Письмо-тест работы smtp-клиента</h2>",
+                Subject = "You are assigned to a test",
+                Body = "<h2>Check your board for pass a test</h2>",
                 IsBodyHtml = true
             };
             var smtp = new SmtpClient("smtp.gmail.com", 587)
             {
-                Credentials = new NetworkCredential("somemail@gmail.com", "mypassword"),
+                Credentials = new NetworkCredential("athleteunitelyselenidetests@gmail.com", "Z123456x"),
                 EnableSsl = true
             };
 
             await smtp.SendMailAsync(m);
-            Console.WriteLine("Письмо отправлено");
         }
     }
 }

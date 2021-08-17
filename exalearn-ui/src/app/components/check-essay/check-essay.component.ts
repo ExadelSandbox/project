f3d3982a8b77dea1bf0ef6d13ab0e72b73e6ebd7
KotdenGit <kotden09@gmail.com>
2021-08-17 18:01:07 +0300

    [6-3]feat: add check-essay module files

---
+++ exalearn-ui/src/app/components/check-essay/check-essay.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-check-essay',
+	templateUrl: './check-essay.component.html',
+	styleUrls: ['./check-essay.component.scss']
+})
+export class CheckEssayComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}

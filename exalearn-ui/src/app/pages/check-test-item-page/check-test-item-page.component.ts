d9f59b202ec5afe8ed3be174da7ba1edaca14c09
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-08-19 00:34:53 +0300

    1. Refactored code

--- exalearn-ui/src/app/pages/check-test-item-page/check-test-item-page.component.ts
+++ exalearn-ui/src/app/pages/check-test-item-page/check-test-item-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { API_PATH } from '../../constants/api.constants';
-import { CheckCoach } from '../../interfaces/interfaces';
+import { CheckCoach, CheckCoachQuestion } from '../../interfaces/interfaces';
 
 @Component({
 	selector: 'app-check-test-item-page',
@@ -14,6 +14,10 @@ export class CheckTestItemPageComponent implements OnInit {
 	dataCheckTest: any;
 	data: CheckCoach;
 
+	public testGrammar: CheckCoachQuestion[] = [];
+	public testAudition: CheckCoachQuestion[] = [];
+	public testTopic: CheckCoachQuestion[] = [];
+
 	constructor(private router: Router, private apiService: ApiService) {
 		//this.passedTestId = this?.router?.getCurrentNavigation()?.extras?.state?.data.id;
 		this.passedTestId = 1;
@@ -21,29 +25,54 @@ export class CheckTestItemPageComponent implements OnInit {
 
 	ngOnInit() {
 		this.apiService.getRequest(API_PATH.GET_CHECK_TEST + `${this.passedTestId}`).then((data) => {
-			console.log(data);
-			// this.dataCheckTest = data;
 			data.userAnswers.forEach((el: any) => {
-				console.log(el);
+				el.questionText = el.question.questionText;
+				switch (el.question.questionType) {
+					case 1:
+						this.getCheckQuestionGrammar(el);
+						break;
+					case 2:
+						this.getCheckQuestionAudition(el);
+						break;
+					case 3:
+						this.getCheckQuestionTopic(el);
+						break;
+				}
 			});
-			//data.forEach((el: any) => {
-			//el.questionText = el.question.questionText;
-			//switch (el.question.questionType) {
-			//	case 1:
-			//this.getCheckQuestionGrammar(el);
-			//		break;
-			//}
-			//});
 		});
 	}
-	//getCheckQuestionGrammar(item: any) {
-	//	item.answerOption = [];
-	//	item.choices.forEach((answer: any) => {
-	//		item.answerOption.push(answer);
-	//		if (answer.isCorrect) {
-	//			item.rightAnswer = answer;
-	//		}
-	//	});
-	//	delete item.question;
-	//}
+
+	getCheckQuestionGrammar(item: any) {
+		item.answerOption = [];
+		item.question.choices.forEach((answer: any) => {
+			item.answerOption.push(answer);
+			if (answer.isCorrect) {
+				item.rightAnswer = answer;
+			}
+		});
+		console.log(item);
+		delete item.question;
+	}
+
+	getCheckQuestionAudition(item: any) {
+		item.answerOption = [];
+		item.question.choices.forEach((answer: any) => {
+			item.answerOption.push(answer);
+			if (answer.isCorrect) {
+				item.rightAnswer = answer;
+			}
+		});
+		delete item.question;
+	}
+
+	getCheckQuestionTopic(item: any) {
+		item.answerOption = [];
+		item.question.choices.forEach((answer: any) => {
+			item.answerOption.push(answer);
+			if (answer.isCorrect) {
+				item.rightAnswer = answer;
+			}
+		});
+		delete item.question;
+	}
 }

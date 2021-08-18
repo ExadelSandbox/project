19764eb0ce909c381cc5be1b2c825e0b021d8c42
KotdenGit <kotden09@gmail.com>
2021-08-18 23:29:01 +0300

    [7-14]feat: test-check grammar integration

--- exalearn-ui/src/app/pages/check-test-item-page/check-test-item-page.component.ts
+++ exalearn-ui/src/app/pages/check-test-item-page/check-test-item-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { API_PATH } from '../../constants/api.constants';
+import { CheckQuestion } from '../../interfaces/interfaces';
 
 @Component({
 	selector: 'app-check-test-item-page',
@@ -18,9 +19,28 @@ export class CheckTestItemPageComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		void this.apiService.getRequest(API_PATH.GET_CHECK_TEST, { passedTestId: this.passedTestId }).then((data) => {
+		this.apiService.getRequest(API_PATH.GET_CHECK_TEST, { passedTestId: this.passedTestId }).then((data) => {
 			this.dataCheckTest = data;
+			//data.forEach((el: any) => {
+			//el.questionText = el.question.questionText;
+			//switch (el.question.questionType) {
+			//	case 1:
+			//this.getCheckQuestionGrammar(el);
+			//		break;
+			//}
+			//});
+
 			console.log(data);
 		});
 	}
+	//getCheckQuestionGrammar(item: any) {
+	//	item.answerOption = [];
+	//	item.choices.forEach((answer: any) => {
+	//		item.answerOption.push(answer);
+	//		if (answer.isCorrect) {
+	//			item.rightAnswer = answer;
+	//		}
+	//	});
+	//	delete item.question;
+	//}
 }

b0f6aff9291b00abb89ccd4cd26578d3a33951f7
Yury Hreben <urikgreben@gmail.com>
2021-08-19 14:36:07 +0300

    [7-53] UI: Translate mark module

--- exalearn-ui/src/app/components/mark/mark.module.ts
+++ exalearn-ui/src/app/components/mark/mark.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
 import { MarkComponent } from './mark.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
 	declarations: [MarkComponent],
-	imports: [CommonModule, MatButtonModule],
+	imports: [CommonModule, MatButtonModule, TranslateModule],
 	exports: [MarkComponent]
 })
 export class MarkModule {}

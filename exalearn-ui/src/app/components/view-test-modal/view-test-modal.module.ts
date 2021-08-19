79b76402ffd3a52235e75a83b5eb4ffe16c48599
Yury Hreben <urikgreben@gmail.com>
2021-08-20 02:27:37 +0300

    [7-60] UI: Improve view test  modal

--- exalearn-ui/src/app/components/view-test-modal/view-test-modal.module.ts
+++ exalearn-ui/src/app/components/view-test-modal/view-test-modal.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ViewTestModalComponent } from './view-test-modal.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [ViewTestModalComponent],
-	imports: [BrowserModule, TranslateModule, MatButtonModule]
+	imports: [BrowserModule, TranslateModule, MatButtonModule, MatIconModule]
 })
 export class ViewTestModalModule {}

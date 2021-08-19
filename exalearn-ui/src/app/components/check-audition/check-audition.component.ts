849531bcc10fe558b61444d478cf9402d0ee3341
KotdenGit <kotden09@gmail.com>
2021-08-19 17:53:58 +0300

    [7-14]feat: file output for audio player

--- exalearn-ui/src/app/components/check-audition/check-audition.component.ts
+++ exalearn-ui/src/app/components/check-audition/check-audition.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { CheckCoach, CheckCoachQuestion, CheckQuestion, Question } from 'src/app/interfaces/interfaces';
+import { CheckCoachQuestion, Question } from 'src/app/interfaces/interfaces';
 import { AudioCloudService } from '../../services/audio-cloud.service';
 
 @Component({
@@ -16,7 +16,6 @@ export class CheckAuditionComponent implements OnInit {
 	constructor(private cloudService: AudioCloudService) {}
 
 	ngOnInit(): void {
-		console.log(this.testAuditionAnswers);
 		this.cloudService.setFiles(
 			'https://6a63fca904fd268f15f7-d5770ffdd579eb31eaa89faeffc55fe7.ssl.cf1.rackcdn.com/Audio_zone-Music_in_Manchester.mp3'
 		);

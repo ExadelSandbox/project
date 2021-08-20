ebb663ae9341bdbca1b17a9de2c0d87757245ed4
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-08-20 09:30:38 +0300

    1. Refactored code

--- exalearn-ui/src/app/components/submit-checked-test-button/submit-checked-test-button.component.ts
+++ exalearn-ui/src/app/components/submit-checked-test-button/submit-checked-test-button.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { AuditionService } from '../../services/audition.service';
+import { ApiService } from '../../services/api.service';
+import { API_PATH } from '../../constants/api.constants';
 
 @Component({
 	selector: 'app-submit-checked-test-button',
@@ -6,7 +12,29 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./submit-checked-test-button.component.scss']
 })
 export class SubmitCheckedTestButtonComponent implements OnInit {
-	constructor() {}
+	@Input() passedTestId: number | undefined;
+	@Input() essayMark: number | undefined;
+	@Input() speakingMark: number | undefined;
+
+	constructor(
+		public dialog: MatDialog,
+		public router: Router,
+		public translateService: TranslateService,
+		public auditionService: AuditionService,
+		private apiService: ApiService
+	) {}
 
 	ngOnInit(): void {}
+
+	public async submitTest() {
+		const bodyAssessment = {
+			'passedTestId': this.passedTestId,
+			'essay': this.essayMark,
+			'speaking': this.speakingMark
+		};
+
+		this.auditionService.submitTestPause();
+		void (await this.apiService.postRequest(API_PATH.TEST_ASSESSMENT, bodyAssessment));
+		void (await this.router.navigate(['/main']));
+	}
 }
